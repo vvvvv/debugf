5db997f98077998505788f4df67f628ea641f467
vvvvv <vvvvv@vvvvv.io>
2021-07-28 00:01:59 +0200

    readme

--- delog.go
+++ delog.go
@@ -1,9 +1,9 @@
-// Package delog is useful for development only
-// It provides helpers such as debug printers and such
+// Package delog is useful for printing debug messages
 package delog
 
 // Printf formats exactly like fmt.Printf does but writes to stderr.
 // This function will only print if build with `go build -flags delog` and is a noop otherwise.
+
 // go:noinline
 var Printf PrintfFunc = func(_ string, _ ...interface{}) {}
 
